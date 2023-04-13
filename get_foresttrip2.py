69f89125f126daf92003ab954b7cdb0655b6cbf5
net384 <net384go@gmail.com>
2023-04-13 14:43:25 +0900

    ㅇㅇ
    ㄴㅇ

--- get_foresttrip2.py
+++ get_foresttrip2.py
@@ -15,7 +15,8 @@ import sys
 ##날짜 입력으로 seq 값 확인\\\
 d = {}
 ## with open("get_foresttrip.in") as f:
-with open("/root/script/get_foresttrip2.in") as f:
+##with open("/root/script/get_foresttrip.in") as f:
+with open("get_foresttrip.in") as f:
     for line in f:
         (key, val) = line.split('=')
         d[str(key)] = val
@@ -56,22 +57,17 @@ houseCampSctin : 01 휴양림, 02 야영
 부산/경남 : _csrf: e122ef39-c336-4eca-b476-9f854f436683, srchInsttArcd: 8
 제주 : _csrf : a9f5b305-4658-4bf6-a516-e53d9409bc5c, srchInsttArcd : 9
 
-
-
-
-
-
 '''
 area = {
         '인천/경기'		: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 1},
         '강원'			: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 2},
         '충북'			: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 3},
-                '전북'			: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 5},
+        '전북'			: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 5},
         '전남'			: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 6},
-                '대구/경북'		: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 7},
+        '대구/경북'		: {'_csrf': 'b40b2846-a44e-4b96-81b7-5bf8c9140501' , 'srchInsttArcd': 7},
         '부산/경남'		: {'_csrf': 'e122ef39-c336-4eca-b476-9f854f436683' , 'srchInsttArcd': 8},
-                '제주'			: {'_csrf': 'a9f5b305-4658-4bf6-a516-e53d9409bc5c' , 'srchInsttArcd': 9},
-                        '대전/충남'		: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 4},
+        '제주'			: {'_csrf': 'a9f5b305-4658-4bf6-a516-e53d9409bc5c' , 'srchInsttArcd': 9},
+        '대전/충남'		: {'_csrf': 'd7e59d7a-f836-45b7-a985-f56585ed3af1' , 'srchInsttArcd': 4},
         }
 
 
@@ -81,7 +77,7 @@ for i,val in area.items():
     #time.sleep(2)
     
     location = i
-    
+    '''
     paramDict = {  '_csrf' : area[i]['_csrf'] ,
                     'srchInsttArcd' : area[i]['srchInsttArcd'] ,
                     'srchInsttId' :'',
@@ -111,7 +107,54 @@ for i,val in area.items():
                     'srchGoodsId' : '',
                     'hmpgId' : 'FRIP',  
                  } 
-    
+    '''
+
+    #23.4.13 넷퍼넬 도입으로 인한 네퍼넬 키 요청 로직 추가
+    nef_url = "https://nf.foresttrip.go.kr/ts.wseq?opcode=5101&nfid=0&prefix=NetFunnel.gRtype=5101;&sid=service_1&aid=action1&js=yes&1681360392254" 
+    response = requests.get(nef_url)
+    string = response.text
+    #print(response.text)
+
+    ## 넷퍼넬 콜백에서 난수 키값을 받아 온다.
+    try:
+        found = re.search('key=(.+?)&nwait', string).group(1)
+        #print(found)
+    except AttributeError:
+        print("[01FC] Cann't find netfernel key value.")
+        pass
+
+    paramDict = {
+        '_csrf': area[i]['_csrf'],
+        'srchInsttArcd': area[i]['srchInsttArcd'] ,
+        'srchInsttId': '',
+        'srchRsrvtBgDt': day1,
+        'srchRsrvtEdDt': day2,
+        'srchStngNofpr': 2,
+        'srchSthngCnt': 1,
+        'srchWord': '',
+        'netfunnel_key': found,
+        'houseCampSctin': '01',
+        'rsrvtPssblYn': '',
+        'rsrvtWtngSctin': '01',
+        'srchHouseCharg': '',
+        'srchCampCharg': '',
+        'goodsClsscHouseCdArr': '',
+        'goodsClsscCampCdArr': '',
+        'srchInsttTpcd': '',
+        'cmdogYn': 'N',
+        'bbqYn': 'N',
+        'dsprsYn': 'N',
+        'otsdWeterYn': 'N',
+        'wifiYn': 'N',
+        'snowPlaceYn': 'N',
+        'srchMyLtd': '',
+        'srchMyLng': '',
+        'srchDstnc': '',
+        'gNowPage': 1,
+        'srchGoodsId': '',
+        'hmpgId': 'FRIP',
+    }
+
     url = "https://www.foresttrip.go.kr/rep/or/fcfsRsrvtRcrfrDtlDetls.do" 
     
     response = requests.get(url, params=paramDict) 
@@ -148,5 +191,7 @@ for i,val in area.items():
             ',day='+day1[6:8]+',vacancy='+x.text.replace('예약가능 객실 수 : ',''))
     else : 
         print(response.status_code)
+    
+    time.sleep(2)
 
 
